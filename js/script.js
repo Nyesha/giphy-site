349dc102adbe10350ade1f026227586ac20bcc55
Nyesha Jones <neeshajolmfao@gmail.com>
2019-04-08 22:24:05 +0000

    We Were Late

--- js/script.js
+++ js/script.js
@@ -2,8 +2,18 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
+var api_url= "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC"
+
+
 
 $("#search-button").click(function(){
+    $.ajax({
+        url: api_url,
+        method: "GET",
+        success: function(response) {
+            
+            
+        }
   
   
   
