edcb49acf94a8d45ade4c9ce86462fe60aad428c
Nyesha Jones <neeshajolmfao@gmail.com>
2019-04-15 22:07:31 +0000

    Good Things

--- js/script.js
+++ js/script.js
@@ -2,18 +2,20 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-var api_url= "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC"
-
+var custom_api_url = "https://api.giphy.com/v1/gifs/search?q="
+var api_key = "&api_key=dc6zaTOxFJmzC"
 
 
 $("#search-button").click(function(){
+    var searchTerm = $("#search-term").val();
     $.ajax({
-        url: api_url,
+        url: custom_api_url + searchTerm + api_key,
         method: "GET",
         success: function(response) {
-            
+            $(".gallery").html("<img src='" + response.data[4].images.original.url + "'>" );
             
         }
+    });
   
   
   
